6b58d76d705f8d67e54a326cd3e2d50339672241
marcomarcellis <m.m.c.m.marcellis@hva.nl>
2020-09-27 19:24:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/madlevel7task1/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.madlevel7task1
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Use Handler to wait 1 second before opening the CreateProfileActivity.
+        Handler().postDelayed({
+            startActivity(
+                Intent(
+                    this@SplashActivity,
+                    MainActivity::class.java
+                )
+            )
+            finish()
+        }, 1000)
+    }
+}
